f8374a30bd687567b3876ad2ab9ebd3db88721f5
Daniel <daniel.alamo@gmail.com>
2016-09-21 19:57:10 -0400

    Update TaxServiceRest.php for Windows

--- src/AvaTax/TaxServiceRest.php
+++ src/AvaTax/TaxServiceRest.php
@@ -91,8 +91,8 @@ class TaxServiceRest
 		curl_setopt($curl, CURLOPT_USERPWD, $this->config['account'].":".$this->config['license']);
 
 		//Some Windows users have had trouble with our SSL Certificates. Uncomment the following line to NOT use SSL.
-        // *This is not recommended, see below for better alternative*
-		//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); 		
+        	// *This is not recommended, see below for better alternative*
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); 		
 		
         //Other Windows users may prefer to download the certificate from our site (detail here: http://developer.avalara.com/api-docs/designing-your-integration/errors-and-outages/ssl-certificates) and manually set the cert path.
 		//    To set the path manually, uncomment the following two lines and ensure you are telling curl where it can find the root certificate. If you choose to manually set the path, make sure you have reenabled cURL by commenting out the line above 
